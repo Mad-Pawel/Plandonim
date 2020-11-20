74b6ddce19749da8c9bf17cbf2c4c7b5d88748c6
PAWEŁ <pawel.mlo@op.pl>
2020-11-20 14:50:07 +0100

    funkcja czyZdanieJestPlandonimem v1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,23 @@ public class Main {
 
     static void czyZdanieJestPlandonimem(String x)
     {
+        boolean isPl = false;
+        for(int i = 0; i < x.length();i++)
+        {
+            for (int j =x.length(); j >0; j--)
+            {
+                if(x[i] == x[j])
+                    isPl = true;
+                else
+                {
+                    isPl = false;
+                    break;
+                }
 
+            }
+            if(!isPl)
+                break;
+        }
     }
 
     public static void main(String[] args) {
