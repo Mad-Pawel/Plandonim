7100156925326c84b14c0a981ba84b9b4274ab26
PAWEŁ <pawel.mlo@op.pl>
2020-11-20 14:44:30 +0100

    podstawy

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    static void czyZdanieJestPlandonimem(String x)
+    {
+
+    }
+
+    public static void main(String[] args) {
+
+	String nazwa =  "Ala";
+
+
+
+    }
+}
